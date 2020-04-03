bd6d4af18e837f0a902a9e2bd1849618df74a7c0
Mike Elsmore <mike.elsmore@logz.io>
2020-04-03 14:27:50 +0100

    Move the scroll command to it's own function

---
+++ commands/scroll.js
@@ -0,0 +1,27 @@
+const ScrollValidator = require('../validators/scroll');
+const axios = require('../helpers/axios');
+const displayHits = require('../helpers/displayHits');
+
+module.exports = (query, from, size, sort, _source, post_filter, scroll, scrollId) => {
+    const data = {};
+    if (query) data.query = (typeof query === 'object') ? query : JSON.parse(query);
+    if (from) data.from = from;
+    if (size) data.size = size;
+    if (sort) data.sort = sort;
+    if (_source) data._source = (''+_source).split(',');
+    if (post_filter) data.post_filter = post_filter;
+    const { error } = ScrollValidator.validate(data);
+
+    if (error) {
+        debug.log('Please provide valid parts of a request');
+        debug.error(error);
+        process.exit(1);
+    }
+    const config = {};
+    return axios.post('/scroll', data, config)
+        .then(response => {
+            const { scrollId, hits } = response.data;
+            debug.log('Scroll ID: %s', scrollId);
+            displayHits(JSON.parse(hits));
+        });
+}
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 #! /usr/bin/env node
-const Conf = require('conf');
+// CLI framework
 const Fire = require('js-fire');
-const debug = require('./helpers/debug');
 // Commands 
 const ConfigCommand = require('./commands/config');
 const SearchCommand = require('./commands/search');
-// Validators
-const ScrollValidator = require('./validators/scroll');
+const ScrollCommand = require('./commands/scroll');
 
 // I command thee
 const commands = {
@@ -17,29 +15,7 @@ const commands = {
     // Basic Search
     search: SearchCommand,
     // Scroll
-    // scroll: (query, from, size, sort, _source, post_filter, scroll, scrollId) => {
-    //     const data = {};
-    //     if (query) data.query = (typeof query === 'object') ? query : JSON.parse(query);
-    //     if (from) data.from = from;
-    //     if (size) data.size = size;
-    //     if (sort) data.sort = sort;
-    //     if (_source) data._source = (''+_source).split(',');
-    //     if (post_filter) data.post_filter = post_filter;
-    //     const { error } = ScrollValidator.validate(data);
-
-    //     if (error) {
-    //         debug.log('Please provide valid parts of a request');
-    //         debug.error(error);
-    //         process.exit(1);
-    //     }
-    //     const config = {};
-    //     return axios.post('/scroll', data, config)
-    //         .then(response => {
-    //             const { scrollId, hits } = response.data;
-    //             debug.log('Scroll ID: %s', scrollId);
-    //             displayHits(JSON.parse(hits));
-    //         });
-    // }
-}
+    scroll: ScrollCommand,
+};
 
 Fire(commands);
